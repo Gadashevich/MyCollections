b43a56a7ce7fc0fc0acd3108cd55e72a62bfc9c9
GadashJohn <fox5b21@gmail.com>
2022-07-03 22:47:31 +0300

    add new Test's in class QueueTest

--- src/test/java/structure/QueueTest.java
+++ src/test/java/structure/QueueTest.java
@@ -33,14 +33,44 @@ private Queue<String> stringQueue;
     }
 
     @Test
+    void putNullException(){
+        assertThrows(IllegalArgumentException.class, () -> stringQueue.put(null));
+    }
+
+    @Test
+    void  putNullMessage(){
+        try{
+            stringQueue.put(null);
+            fail("No Exeption");
+        } catch(IllegalArgumentException e) {
+            assertEquals("Element can not be null", e.getMessage());
+        }
+    }
+
+    @Test
     void getFirstElement() {
+     fillQueue();
+     assertEquals(4,stringQueue.size());
+     assertEquals("its_First_StringElement",stringQueue.getFirstElement());
+     assertEquals("its_First_StringElement",stringQueue.getFirstElement());
+     assertEquals(4,stringQueue.size());
     }
 
     @Test
     void getLastElement() {
+        fillQueue();
+        assertEquals(4,stringQueue.size());
+        assertEquals("its`Four_StringElement",stringQueue.getLastElement());
+        assertEquals("its`Four_StringElement",stringQueue.getLastElement());
+        assertEquals(4,stringQueue.size());
     }
 
     @Test
     void get() {
+        assertNull(stringQueue.get());
+        assertEquals(0,stringQueue.size());
     }
+
+
+
 }
